94654a45e3aa9b3e647388f0471a036cb28ac1fe
2ran <brakemire@yandex.ru>
2023-10-19 22:14:22 +0300

    fix repo

--- stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
@@ -3,19 +3,18 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.model.EndpointHit;
-import ru.practicum.model.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EndPointHitRepository extends JpaRepository<EndpointHit, Long> {
-    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
-            "from EndPointHit as h where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
-            "group by h.app, h.uri, h.ip order by count(h.ip) desc")
-    List<ViewStats> getStats(List<String> uris, LocalDateTime start, LocalDateTime end);
+    List<EndpointHit> findAllByTimestampBetweenAndUriIn(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
-            "from EndPointHit h where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
-            "group by h.app, h.uri, h.ip order by count(distinct h.ip) desc")
-    List<ViewStats> getStatsUnique(List<String> uris, LocalDateTime start, LocalDateTime end);
+    @Query("SELECT COUNT (ip) FROM Hit " +
+            "WHERE uri = ?1")
+    Long findHitCountByUri(String uri);
+
+    @Query("SELECT COUNT (DISTINCT ip) FROM Hit " +
+            "WHERE uri = ?1")
+    Long findHitCountByUriWithUniqueIp(String uri);
 }

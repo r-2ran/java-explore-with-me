ecfe48c1fec04ab7899bf440e33b5ff580ddbdb5
2ran <brakemire@yandex.ru>
2023-10-19 21:38:42 +0300

    fix repo

--- stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
@@ -11,11 +11,11 @@ import java.util.List;
 public interface EndPointHitRepository extends JpaRepository<EndpointHit, Long> {
     @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
             "from EndPointHit as h where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
-            "group by h.app, h.uri, h.ip order by count(h.ip) desc", nativeQuery = true)
+            "group by h.app, h.uri, h.ip order by count(h.ip) desc")
     List<ViewStats> getStats(List<String> uris, LocalDateTime start, LocalDateTime end);
 
     @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
             "from EndPointHit h where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
-            "group by h.app, h.uri, h.ip order by count(distinct h.ip) desc", nativeQuery = true)
+            "group by h.app, h.uri, h.ip order by count(distinct h.ip) desc")
     List<ViewStats> getStatsUnique(List<String> uris, LocalDateTime start, LocalDateTime end);
 }

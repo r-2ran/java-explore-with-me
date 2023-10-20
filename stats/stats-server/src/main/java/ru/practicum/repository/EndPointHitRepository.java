89d3933986378b12ce1db4decca881c23186a42f
2ran <brakemire@yandex.ru>
2023-10-20 11:19:23 +0300

    fix repo

--- stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/EndPointHitRepository.java
@@ -3,28 +3,37 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.model.EndpointHit;
+import ru.practicum.model.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EndPointHitRepository extends JpaRepository<EndpointHit, Long> {
-    List<EndpointHit> findAllByTimestampBetweenAndUriIn(LocalDateTime start, LocalDateTime end, List<String> uris);
+    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
+            "from EndpointHit h " +
+            "where h.timestamp between ?1 and ?2 " +
+            "group by h.app, h.uri, h.ip " +
+            "order by count(distinct h.ip) desc")
+    List<ViewStats> findAllUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    List<EndpointHit> findAllByTimestampBetween(LocalDateTime start, LocalDateTime end);
+    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(h.app)) " +
+            "from EndpointHit h " +
+            "where h.timestamp between ?1 and ?2 " +
+            "group by h.app, h.uri " +
+            "order by count(h.app) desc")
+    List<ViewStats> findAllNotUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT COUNT (ip) FROM EndpointHit " +
-            "WHERE uri = ?1")
-    Long findHitCountByUri(String uri);
+    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) " +
+            "from EndpointHit h " +
+            "where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
+            "group by h.app, h.uri, h.ip " +
+            "order by count(distinct h.ip) desc")
+    List<ViewStats> findUniqueIpByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT COUNT (ip) FROM EndpointHit " +
-            "GROUP BY uri")
-    Long findHitCount();
-
-    @Query("SELECT COUNT (DISTINCT ip) FROM EndpointHit " +
-            "WHERE uri = ?1")
-    Long findHitCountByUriWithUniqueIp(String uri);
-
-    @Query("SELECT COUNT (DISTINCT ip) FROM EndpointHit " +
-            "GROUP BY uri")
-    Long findHitCountWithUniqueIp();
+    @Query(value = "select new ru.practicum.model.ViewStats(h.app, h.uri, count(h.app)) " +
+            "from EndpointHit h " +
+            "where h.timestamp between ?1 and ?2 and h.uri in ?3 " +
+            "group by h.app, h.uri " +
+            "order by count(h.app) desc")
+    List<ViewStats> findNotUniqueIpByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
--- stats/stats-server/src/main/java/ru/practicum/service/EndPointHitServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/EndPointHitServiceImpl.java
@@ -5,14 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStatsDto;
-import ru.practicum.model.EndpointHit;
 import ru.practicum.model.ViewStats;
 import ru.practicum.repository.EndPointHitRepository;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static ru.practicum.mapper.EndPointHitMapper.*;
 import static ru.practicum.mapper.ViewStatsMapper.*;
@@ -35,45 +33,21 @@ public class EndPointHitServiceImpl implements EndPointHitService {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime startDate = LocalDateTime.parse(start, formatter);
         LocalDateTime endDate = LocalDateTime.parse(end, formatter);
-        List<EndpointHit> hits;
-        List<ViewStats> viewStats = new ArrayList<>();
+        List<ViewStats> res;
 
-        if (uris == null) {
-            hits = repository.findAllByTimestampBetween(startDate, endDate);
-            for (EndpointHit hit : hits) {
-                Long hitCount;
-                if (isUnique) {
-                    hitCount = repository.findHitCountWithUniqueIp();
-                } else {
-                    hitCount = repository.findHitCount();
-                }
-                viewStats.add(new ViewStats(hit.getApp(), hit.getUri(), hitCount));
+        if (isUnique) {
+            if (uris == null) {
+                res = repository.findAllUniqueIp(startDate, endDate);
+            } else {
+                res = repository.findUniqueIpByUris(startDate, endDate, uris);
             }
         } else {
-            hits = repository.findAllByTimestampBetweenAndUriIn(startDate, endDate, uris);
-            for (EndpointHit hit : hits) {
-                Long hitCount;
-                if (isUnique) {
-                    hitCount = repository.findHitCountByUriWithUniqueIp(hit.getUri());
-                } else {
-                    hitCount = repository.findHitCountByUri(hit.getUri());
-                }
-                viewStats.add(new ViewStats(hit.getApp(), hit.getUri(), hitCount));
+            if (uris == null) {
+                res = repository.findAllNotUniqueIp(startDate, endDate);
+            } else {
+                res = repository.findNotUniqueIpByUris(startDate, endDate, uris);
             }
         }
-        viewStats = viewStats
-                .stream()
-                .sorted(Comparator.comparingLong(ViewStats::getHits).reversed())
-                .collect(Collectors.toList());
-        if (!isUnique) {
-            for (int i = 0; i < viewStats.size() - 1; i++) {
-                if (viewStats.get(i).getUri().equals(viewStats.get(i + 1).getUri())) {
-                    viewStats.get(i + 1).setHits(viewStats.get(i + 1).getHits() +
-                            viewStats.get(i).getHits());
-                    viewStats.remove(i);
-                }
-            }
-        }
-        return toDtos(viewStats);
+        return toDtos(res);
     }
 }
